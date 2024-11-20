ea4a5479f0b1b9f8db43bb1ce7cfb72f00b84595
JulianL1984 <julianleon666@gmail.com>
2024-11-20 13:12:21 -0500

    Update product-services.js

--- js/services/product-services.js
+++ js/services/product-services.js
@@ -20,9 +20,14 @@ const createProduct = async (name, price, image) => {
       body: JSON.stringify({ name, price, image }),
     });
 
+    // Verificamos si la respuesta es exitosa
+    if (!response.ok) {
+      throw new Error('Error al crear el producto');
+    }
+
     const data = await response.json();
-    console.log("Solicitud POST exitosa:", data);
-    return data;
+    console.log("Producto creado con éxito:", data);
+    return data; // Devolvemos el producto creado
   } catch (error) {
     console.error("Error en la solicitud POST:", error);
   }
@@ -30,12 +35,18 @@ const createProduct = async (name, price, image) => {
 
 const deleteProduct = async (id) => {
   try {
-    await fetch(`${BASE_URL}/${id}`, {
+    const response = await fetch(`${BASE_URL}/${id}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
       },
     });
+
+    // Verificamos si la eliminación fue exitosa
+    if (!response.ok) {
+      throw new Error('Error al eliminar el producto');
+    }
+
     console.log(`Producto con id ${id} eliminado exitosamente`);
   } catch (error) {
     console.error("Error en la solicitud DELETE:", error);
